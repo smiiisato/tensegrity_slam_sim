51088ff8ee2d760df737502a7ac58686f21c3325
smiiisato <sonoda-misato078@g.ecc.u-tokyo.ac.jp>
2023-10-17 17:24:41 +0900

    fix: callback

--- scripts/start_command_callback.py
+++ scripts/start_command_callback.py
@@ -1,8 +1,12 @@
 ## if the reward is beyond the threshold, then start randomizing the command
 
 from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.utils import safe_mean
 
 class StartCommandCallback(BaseCallback):
+    """
+    平均報酬が閾値を超えたら、初期位置のランダム化を開始するコールバック。
+    """
     def __init__(self, threshold, verbose=0):
         super(StartCommandCallback, self).__init__(verbose)
         self.threshold = threshold
@@ -13,7 +17,7 @@ class StartCommandCallback(BaseCallback):
         """
         This method is called before collecting the rollouts.
         """
-        self.ep_rew_mean = self.logger.info("rollout/ep_rew_mean")
+        self.ep_rew_mean = safe_mean([ep_info["r"] for ep_info in self.model.ep_info_buffer])
         if self.ep_rew_mean and (self.ep_rew_mean > self.threshold) and not self.changed:
             self.training_env.env_method("start_randomizing_command")
             self.changed = True
